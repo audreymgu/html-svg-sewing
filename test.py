98cb96f3cc8a70cc0f0532d201226d246c9bb65d
Audrey Gu <audreymgu@gmail.com>
2026-08-10 13:28:48 -0400

    Basic raw output of html markup to sewing machine.

--- test.py
+++ test.py
@@ -1,10 +1,27 @@
 import drawsvg as draw
 
+# pass name of file to script
+# specify drawing area
+# read in the file
+# walk character by character over the output
+# when an opening brace is hit, 
+# grab substring from opening brace to first space
+# ignore any additional parameters attached (e.g. class, href, etc.)
+# pass substring to tag checker function to resolve tag
+# should tag checker be the one to grab the contents, or just pass back formatting information?
+# perhaps this should be a recursive function call?
+# take all text starting from > to </ (this will require additional validation even in the case where we are only evaluating tags at a single level of depth)
+
+
+document = open("test.html", "r")
+content = document.read()
+print(content)
+
 d = draw.Drawing(400, 100, origin='top-left')
 
 
 d.append(draw.Text(
-    text='Hello World', 
+    text=content, 
     font_size=40, 
     x=20, 
     y=65, 
